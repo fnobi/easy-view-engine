59e5ac903472883b98b1df253c33e7fd1a58a8d7
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-01-27 10:25:25 +0900

    現状の機能ざっとテスト

---
+++ test/easy-view-engine-test.js
@@ -0,0 +1,29 @@
+var expect = chai.expect;
+
+describe('easy-view-engine', function () {
+    it('extend variable', function () {
+        var src = '{{=:hoge}}{{=:moge}}';
+        var param = {
+            hoge: 1,
+            moge: 2
+        };
+        var result = easyViewEngine(src, param);
+        expect(result).to.equal('12');
+    });
+    it('handle if statement', function () {
+        var src = '{{if:hoge}}hoge{{endif:hoge}}{{if:moge}}moge{{endif:moge}}';
+        var param = {
+            hoge: true
+        };
+        var result = easyViewEngine(src, param);
+        expect(result).to.equal('hoge');
+    });
+    it('use escape variable and raw variable', function () {
+        var src = '{{=:html}}{{-:html}}';
+        var param = {
+            html: '<a href="http://fnobi.com/">fnobi.com</a>'
+        };
+        var result = easyViewEngine(src, param);
+        expect(result).to.equal('&lt;a href=&quot;http://fnobi.com/&quot;&gt;fnobi.com&lt;/a&gt;<a href="http://fnobi.com/">fnobi.com</a>');
+    });
+});
