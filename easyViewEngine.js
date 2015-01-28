491b5f58d08a7d45b2a34c86bedd19031a526b49
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-01-28 20:21:58 +0900

    ビルドしてなかった。

--- easyViewEngine.js
+++ easyViewEngine.js
@@ -2,7 +2,7 @@ var easyViewEngine = function (source, opts) {
     source = source || '';
     opts = opts || {};
 
-    var regexp = /\{\{(-|=|if|endif):?([a-zA-Z\._]*)\}\}/;
+    var regexp = /\{\{(-|=|if|endif|each|endeach):?([a-zA-Z\._]*)(:[a-zA-Z_]+)?\}\}/;
     var nest = [];
 
     function esc (str) {
@@ -25,10 +25,17 @@ var easyViewEngine = function (source, opts) {
         return ctx[key];
     }
 
-    function processTag (index, command, target) {
+    function assertNest (type) {
+        if (!nest.length || nest[nest.length - 1].type != type) {
+            throw Error('parse error: unexpected end' + type);
+        }
+    }
+
+    function processTag (index, matchLength, command, target, holder) {
         var value = '';
         var raw = false;
         var lastNest = null;
+        var scope, result;
         var undef;
         if (command == '=') {
             value = evl(opts, target);
@@ -39,16 +46,46 @@ var easyViewEngine = function (source, opts) {
             nest.push({
                 index: index,
                 type: 'if',
-                flag: !!evl(opts, target)
+                flag: !!evl(opts, target),
+                scope: source.slice(index + matchLength)
             });
         } else if (command == 'endif') {
-            if (!nest.length || nest[nest.length - 1].type != 'if') {
-                throw Error('parse error: unexpected endif');
-            }
+            assertNest('if');
             lastNest = nest.pop();
-            if (!lastNest.flag) {
-                source = source.slice(0, lastNest.index) + source.slice(index);
+
+            scope = lastNest.scope.slice(0, index - source.length);
+            result = lastNest.flag ? easyViewEngine(scope, opts) : '';
+            source = [
+                source.slice(0, lastNest.index),
+                result,
+                source.slice(index)
+            ].join('');
+        } else if (command == 'each') {
+            nest.push({
+                index: index,
+                type: 'each',
+                scope: source.slice(index + matchLength),
+                ctx: evl(opts, target),
+                holder: holder.slice(1)
+            });
+        } else if (command == 'endeach') {
+            assertNest('each');
+            lastNest = nest.pop();
+
+            scope = lastNest.scope.slice(0, index - source.length);
+            result = '';
+            for (var i = 0; i < lastNest.ctx.length; i++) {
+                (function () {
+                    var ctx = opts;
+                    ctx[lastNest.holder] = lastNest.ctx[i];
+                    result += easyViewEngine(scope, ctx);
+                })();
             }
+            source = [
+                source.slice(0, lastNest.index),
+                result,
+                source.slice(index)
+            ].join('');
         }
         if (!raw) {
             value = esc(value);
@@ -59,7 +96,13 @@ var easyViewEngine = function (source, opts) {
     var matchData;
     while (source.match(regexp)) {
         matchData = source.match(regexp);
-        processTag(matchData.index, matchData[1], matchData[2]);
+        processTag(
+            matchData.index,
+            matchData[0].length,
+            matchData[1],
+            matchData[2],
+            matchData[3]
+        );
     }
 
     return source;
